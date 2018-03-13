ba0881fdc1a12a2bfa2faadfa8b51e12c67d673e
Angel M De Miguel <angel@bitnami.com>
2018-03-13 13:50:45 +0100

    Prepare the deploy.sh script to be executed from the parent directory (hex)

--- build.sh
+++ build.sh
@@ -7,9 +7,6 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && \
   install_packages yarn
 
-# Cd into the correct folder
-cd /app
-
 # Install lerna
 yarn global add lerna && yarn install
 
--- deploy.sh
+++ deploy.sh
@@ -2,26 +2,22 @@
 
 # Get the current git hash
 K8S_ENV=${K8S_ENV:-development}
-REPO_GIT_HASH=`cd ../ && git rev-parse origin/master`
+REPO_GIT_HASH=`git rev-parse origin/master`
 IMAGE=gcr.io/bitnami-containers/hex-docs-$K8S_ENV:$REPO_GIT_HASH
 
 # Build the new image to production
 if [ "$SKIP_IMAGE_BUILDING" != true ]; then
   echo "Building the image..."
   # Build the project
-  dir=$(pwd)
-  docker run --rm -v $dir:/scripts -v $(dirname "$dir"):/app bitnami/node:8 /scripts/build.sh
+  docker run --rm -v $dir:/app bitnami/node:8 /app/hex-deployment/build.sh
 
   if [ $? -ne 0 ]; then
     echo "There was an error building the project. The deployment has been canceled."
     exit 1
   fi
 
-  # Move result
-  cp ../packages/hex-docs/dist ./dist
-
   # Build
-  docker build -t $IMAGE -f deployment/Dockerfile .
+  docker build -t $IMAGE -f hex-deployment/deployment/Dockerfile .
 
   if [ $? -ne 0 ]; then
     echo "There was an error building the image. The deployment has been canceled."
@@ -38,12 +34,12 @@ if [ "$SKIP_IMAGE_BUILDING" != true ]; then
 fi
 
 # Update the deployment!
-cp deployment/deployment.template deployment/deployment.yaml
+cp hex-deployment/deployment/deployment.template hex-deployment/deployment/deployment.yaml
 SAFE_IMAGE_URL=$(echo "$IMAGE" | sed -e 's/\//\\\//g')
-sed -i '' -e "s/PRODUCTION_IMAGE/$SAFE_IMAGE_URL/g" deployment/deployment.yaml
+sed -i '' -e "s/PRODUCTION_IMAGE/$SAFE_IMAGE_URL/g" hex-deployment/deployment/deployment.yaml
 
 # Apply the changes
 echo "Deploying..."
-kubectl apply -f deployment/deployment.yaml
-kubectl apply -f deployment/service.yaml
-kubectl apply -f deployment/ingress.yaml
+kubectl apply -f hex-deployment/deployment/deployment.yaml
+kubectl apply -f hex-deployment/deployment/service.yaml
+kubectl apply -f hex-deployment/deployment/ingress.yaml
